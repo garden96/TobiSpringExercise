f0a876c1b1ecae00734aea7028a1fa8f141da6d4
jeongown.an <garden96@gmail.com>
2020-08-05 04:16:36 +0900

    파일을 열어 모든 라인의 숫자를 더한 합과 곱을 돌려주는 코드로 수정  - 테스트 코드에서 중복 초기 코드를 분리하고 새로운 곱을 구하기 위한 콜백 코드를 추가

--- src/springbook/learningtest/template/CalcSumTest.java
+++ src/springbook/learningtest/template/CalcSumTest.java
@@ -9,13 +9,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CalcSumTest {
+    Calculator calculator;
+    String numFilepath;
 
-	@Test public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
+    @Before public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilepath = getClass().getResource("number.txt").getPath();
+    }
 
-		assertThat(sum, is(10));
+    @Test public void sumOfNumbers() throws IOException {
+		assertThat(calculator.calcSum(this.numFilepath), is(10));
 	}
+
+    @Test public void multiplyOfNumbers() throws IOException {
+        assertThat(calculator.calcMultiply(this.numFilepath), is(10));
+    }
 
 }
 
--- src/springbook/learningtest/template/Calculator.java
+++ src/springbook/learningtest/template/Calculator.java
@@ -24,6 +24,22 @@ public class Calculator {
         return fileReadTemplate(filepath, sumCallback);
     }
 
+    public Integer calcMultiply(String filepath) throws IOException {
+
+        BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                Integer multiply = 0;
+                String line = null;
+                while((line = br.readLine()) != null) {
+                    multiply *= Integer.valueOf(line);
+                }
+                return multiply;
+            }
+        };
+
+        return fileReadTemplate(filepath, multiplyCallback);
+    }
+
 	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
 
 	    BufferedReader br = null;
@@ -43,4 +59,5 @@ public class Calculator {
             }
         }
 	}
+
 }
