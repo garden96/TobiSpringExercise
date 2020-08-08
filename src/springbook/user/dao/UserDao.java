091f1558b2a448abeaad2f1bf5210666822ee804
jeongown.an <garden96@gmail.com>
2020-08-09 06:09:42 +0900

    중복 코드 제거
    get(), getAll() 에서 사용하는 RowMapper는
    중복코드로 코드를 분리해내서 재사용 가능하도록 수정.

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -14,6 +14,18 @@ public class UserDao {
 
     private JdbcTemplate jdbcTemplate;
 
+    private RowMapper<User> userMapper =
+            new RowMapper<User>() {
+                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    User user = new User();
+                    user.setId(rs.getString("id"));
+                    user.setName(rs.getString("name"));
+                    user.setPassword(rs.getString("password"));
+                    return user;
+                }
+            };
+
+
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
@@ -31,15 +43,7 @@ public class UserDao {
 		return this.jdbcTemplate.queryForObject(
 		        "select * from users where id = ?",
                 new Object[] {id},
-                new RowMapper<User>() {
-                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
-                        return user;
-                    }
-		        }
+                this.userMapper
 		);
     }
 
@@ -54,15 +58,7 @@ public class UserDao {
 	public List<User> getAll() {
         return this.jdbcTemplate.query(
                 "select * from users order by id",
-                new RowMapper<User>() {
-                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        User user = new User();
-                    	user.setId(rs.getString("id"));
-                    	user.setName(rs.getString("name"));
-                    	user.setPassword(rs.getString("password"));
-                    	return user;
-                    }
-                }
+                this.userMapper
         );
 	}
 
