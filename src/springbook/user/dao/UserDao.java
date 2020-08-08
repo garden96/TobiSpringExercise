8eaf47c8d1209f39e4679e148cb283fb4fa4f510
jeongown.an <garden96@gmail.com>
2020-08-09 05:40:20 +0900

    불필요한 코드 제거

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -12,11 +12,9 @@ import java.util.List;
 
 public class UserDao {
 
-    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
