42c00d52decea213febe51d64af9159663840e92
jeongown.an <garden96@gmail.com>
2020-08-18 00:05:00 +0900

    사용자 추가 기능 추가
    비지니스 로직에 사용자 추가 기능을 추가하고
    해당 구현 테스트 코드를 추가

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -15,27 +15,32 @@ public class UserService {
 	}
 
 	public void upgradeLevels() {
-		List<User> users = userDao.getAll();
+        List<User> users = userDao.getAll();
 
-		for(User user : users) {
-		    Boolean changed = null;
+        for(User user : users) {
+            Boolean changed = null;
 
-		    if(user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-		        user.setLevel(Level.SILVER);
-		        changed = true;
+            if(user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
             } else if(user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-		        user.setLevel(Level.GOLD);
-		        changed = true;
-            } else if( user.getLevel() == Level.GOLD) {
-		        changed = false;
+                user.setLevel(Level.GOLD);
+                changed = true;
+            } else if(user.getLevel() == Level.GOLD) {
+                changed = false;
             } else {
-		        changed = false;
+                changed = false;
             }
 
-		    if(changed) {
-		        userDao.update(user);
+            if(changed) {
+                userDao.update(user);
             }
-		}
+        }
+    }
+	
+	public void add(User user) {
+		if (user.getLevel() == null) user.setLevel(Level.BASIC);
+		userDao.add(user);
 	}
 
 }
--- src/springbook/user/service/UserServiceTest.java
+++ src/springbook/user/service/UserServiceTest.java
@@ -65,5 +65,22 @@ public class UserServiceTest {
 	    assertThat(userUpdate.getLevel(), is(expectedLevel));
     }
 
+	@Test 
+	public void add() {
+		userDao.deleteAll();
+
+		User userWithLevel = users.get(4);	  // GOLD 레벨
+		User userWithoutLevel = users.get(0);  
+		userWithoutLevel.setLevel(null);
+
+		userService.add(userWithLevel);	  
+		userService.add(userWithoutLevel);
+
+		User userWithLevelRead = userDao.get(userWithLevel.getId());
+		User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+
+		assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel())); 
+		assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
+	}
 }
 
