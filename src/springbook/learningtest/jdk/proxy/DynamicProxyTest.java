82cff608eac3903c28168d1c64edb9835f8bfcf9
jeongown.an <garden96@gmail.com>
2020-09-27 04:33:35 +0900

    Proxy Class 설명을 위한 러닝 테스트 코드 작성   1. target class 와 target class를 위한 interface를 작성하고 테스트 코드 작성   2. proxy class 를 작성하고 테스트 코드를 작성   3. proxy class를 제거하고 danamic proxy 코드를 작성하고 테스트 코드에서 이를 이용도록 변경

--- src/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
+++ src/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -20,37 +20,33 @@ public class DynamicProxyTest {
 		assertThat(hello.sayHi("Toby"), is("Hi Toby"));
 		assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
 
-		// proxy class test
-        Hello proxiedHello = new HelloUppercase(new HelloTarget());
+		Hello proxiedHello = (Hello)Proxy.newProxyInstance(
+				getClass().getClassLoader(),                // 동적으로 생성되는 다이나믹 프록시 클래스의 로딩에 사용할 클래스 로더
+				new Class[] { Hello.class},                 // 구현할 인터페이스
+				new UppercaseHandler(new HelloTarget()));   // 부가기능과 위임 코드를 담은 invacationHandler
+
 		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
 		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
 		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
 	}
 
-	// proxy class
-	static class HelloUppercase implements Hello {
-		Hello hello;
+	// dynamic proxy class
+	static class UppercaseHandler implements InvocationHandler {
+		Hello target;      // object for injection
 
-		public HelloUppercase(Hello hello) {
-			this.hello = hello;
+		private UppercaseHandler(Hello target) {
+			this.target = target;
 		}
 
-		public String sayHello(String name) {
-			return hello.sayHello(name).toUpperCase();
+		public Object invoke(Object proxy, Method method, Object[] args)
+				throws Throwable {
+		    String ret = (String)method.invoke(target, args);    // 타겟으로 위임. 인터페이스의 메소드 호출에 모두 적용됨.
+			return ret.toUpperCase();                            // 부가기능 제공.
 		}
-
-		public String sayHi(String name) {
-			return hello.sayHi(name).toUpperCase();
-		}
-
-		public String sayThankYou(String name) {
-			return hello.sayThankYou(name).toUpperCase();
-		}
-
 	}
 
-	// interface for the target class
-	static interface Hello {
+    // interface for the target class
+    static interface Hello {
 		String sayHello(String name);
 		String sayHi(String name);
 		String sayThankYou(String name);
