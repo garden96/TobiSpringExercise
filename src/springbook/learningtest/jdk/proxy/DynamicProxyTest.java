08e4e65bb35c90ffb43ca3c22b43bef7760ab11e
jeongown.an <garden96@gmail.com>
2020-09-27 03:53:43 +0900

    Proxy Class 설명을 위한 러닝 테스트 코드 작   1. target class 와 target class를 위한 interface를 작성하고 테스트 코드 작성   2. proxy class 를 작성하고 테스트 코드를 작성

--- src/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
+++ src/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -14,10 +14,39 @@ public class DynamicProxyTest {
 	@Test
     // client
 	public void simpleProxy() {
+	    //target class test
 		Hello hello = new HelloTarget();
 		assertThat(hello.sayHello("Toby"), is("Hello Toby"));
 		assertThat(hello.sayHi("Toby"), is("Hi Toby"));
 		assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
+
+		// proxy class test
+        Hello proxiedHello = new HelloUppercase(new HelloTarget());
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+	}
+
+	// proxy class
+	static class HelloUppercase implements Hello {
+		Hello hello;
+
+		public HelloUppercase(Hello hello) {
+			this.hello = hello;
+		}
+
+		public String sayHello(String name) {
+			return hello.sayHello(name).toUpperCase();
+		}
+
+		public String sayHi(String name) {
+			return hello.sayHi(name).toUpperCase();
+		}
+
+		public String sayThankYou(String name) {
+			return hello.sayThankYou(name).toUpperCase();
+		}
+
 	}
 
 	// interface for the target class
