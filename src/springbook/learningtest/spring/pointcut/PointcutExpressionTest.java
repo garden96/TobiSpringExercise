504b80c7256e3a83dfb8c90bfcefeec3e8ac6b73
jeongown.an <garden96@gmail.com>
2020-11-03 08:53:17 +0900

    add test helper method to check if a method is for a pointcut expression

--- src/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
+++ src/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
@@ -9,13 +9,23 @@ import static org.junit.Assert.assertThat;
 public class PointcutExpressionTest {
 
 
+    public void pointcutMatches(String expression, Boolean expected, Class<?> clazz, String methodName, Class<?>... args) throws Exception {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(expression);
+
+        // check if both class filter and method matcher conditions meet.
+        assertThat(pointcut.getClassFilter().matches(clazz)
+                        && pointcut.getMethodMatcher().matches(clazz.getMethod(methodName, args), null),
+                is(expected));
+    }
+
     @Test
     public void methodSignaturePointcut() throws SecurityException, NoSuchMethodException {
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
         pointcut.setExpression(
                 "execution(public int " +
-                "springbook.learningtest.spring.pointcut.Target.minus(int,int) " +
-                "throws java.lang.RuntimeException)");  // method signature for Target class.
+                        "springbook.learningtest.spring.pointcut.Target.minus(int,int) " +
+                        "throws java.lang.RuntimeException)");  // method signature for Target class.
 
         /*
          * compare using class filter and method matcher
