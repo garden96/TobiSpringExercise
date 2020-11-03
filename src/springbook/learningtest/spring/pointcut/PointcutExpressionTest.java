3c832d3d088fc0de016f50719feb440db450ea05
jeongown.an <garden96@gmail.com>
2020-11-03 09:14:49 +0900

    test pointcut expression

--- src/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
+++ src/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
@@ -7,6 +7,35 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class PointcutExpressionTest {
+    @Test
+    public void pointcut() throws Exception {
+        tagetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+        tagetClassPointcutMatches("execution(* hello(..))", true, true, false, false, false, false);
+        tagetClassPointcutMatches("execution(* hello())", true, false, false, false, false, false);
+        tagetClassPointcutMatches("execution(* hello(String))", false, true, false, false, false, false);
+        tagetClassPointcutMatches("execution(* meth*(..))", false, false, false, false, true, true);
+
+        tagetClassPointcutMatches("execution(* *(int,int))", false, false, true, true, false, false);
+        tagetClassPointcutMatches("execution(* *())", true, false, false, false, true, true);
+
+        tagetClassPointcutMatches("execution(* springbook.learningtest.spring.pointcut.Target.*(..))", true, true, true, true, true, false);
+        tagetClassPointcutMatches("execution(* springbook.learningtest.spring.pointcut.*.*(..))", true, true, true, true, true, true);
+        tagetClassPointcutMatches("execution(* springbook.learningtest.spring.pointcut..*.*(..))", true, true, true, true, true, true);
+        tagetClassPointcutMatches("execution(* springbook..*.*(..))", true, true, true, true, true, true);
+
+        tagetClassPointcutMatches("execution(* com..*.*(..))", false, false, false, false, false, false);
+        tagetClassPointcutMatches("execution(* *..Target.*(..))", true, true, true, true, true, false);
+        tagetClassPointcutMatches("execution(* *..Tar*.*(..))", true, true, true, true, true, false);
+        tagetClassPointcutMatches("execution(* *..*get.*(..))", true, true, true, true, true, false);
+        tagetClassPointcutMatches("execution(* *..B*.*(..))", false, false, false, false, false, true);
+
+        tagetClassPointcutMatches("execution(* *..TargetInterface.*(..))", true, true, true, true, false, false);
+
+        tagetClassPointcutMatches("execution(* *(..) throws Runtime*)", false, false, false, true, false, true);
+
+        tagetClassPointcutMatches("execution(int *(..))", false, false, true, true, false, false);
+        tagetClassPointcutMatches("execution(void *(..))", true, true, false, false, true, true);
+    }
 
     public void tagetClassPointcutMatches(String expression, boolean... expected) throws Exception {
         pointcutMatches(expression, expected[0], Target.class, "hello");
