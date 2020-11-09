ec1e8f5218683c222f2616c41fc336928239b1f3
jeongown.an <garden96@gmail.com>
2020-11-09 15:29:24 +0900

    UserService 에 메소드 추가하고 해당 메소드 구현
    트랜잭션 경계설정의 일원화를 위해
    UserService 에 메소드를 추가하고
    UserServiceImpl 에서 추가된 메소드 구현.

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -2,8 +2,14 @@ package springbook.user.service;
 
 import springbook.user.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     void add(User user);
+    void deleteAll();
+    void update(User user);
+    User get(String id);
+    List<User> getAll();
 
     void upgradeLevels();
 }
