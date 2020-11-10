36c0f0f87e8ff757b0b27ecc62ce25b5e3be4219
jeongown.an <garden96@gmail.com>
2020-11-10 10:34:15 +0900

    기존 설정을 @Transactional annotation을 사용하도록 수정
    <tx:method name="get*" read-only="true"/> 대신 해당하는 method 앞에 해당기능을 하는 annotaion 적용
    annotaion을 interface 내의 method에 적용하여 구현하는 모든 methoad에 동일 기능을 적용함.
    cf) 만약 구현체 내의 method에 다른 속성이 다른 동일한 annotaion이 적용된다면 구현체에 적용된 annotaion이 우선순위를 가진다.

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -1,5 +1,6 @@
 package springbook.user.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import springbook.user.domain.User;
 
 import java.util.List;
@@ -8,7 +9,11 @@ public interface UserService {
     void add(User user);
     void deleteAll();
     void update(User user);
+
+    @Transactional(readOnly=true)
     User get(String id);
+
+    @Transactional(readOnly=true)
     List<User> getAll();
 
     void upgradeLevels();
