122b4b7afa8d8c62d4de3d36358c31d46d3a8c16
jeongown.an <garden96@gmail.com>
2020-11-11 17:05:32 +0900

    Transaction 이해를 위한 테스트 코드 3 (트랜잭션 동기화 테스트 1)
     - 통합된 트랜잭션임을 테스트 하기위해서 transaction 속성을 readOnly 로 변경(218행)하고,
       224행 실행시 TransientDataAccessResourceException이 발생하여 테스트에 실패함을 확인.

--- src/springbook/user/service/UserServiceTest.java
+++ src/springbook/user/service/UserServiceTest.java
@@ -215,6 +215,8 @@ public class UserServiceTest {
     @Test
     public void transactionSync() {
         DefaultTransactionDefinition txDefinition = new DefaultTransactionDefinition();     // 기본값을 사용하여 트랜잭션정의를 생성
+        txDefinition.setReadOnly(true);                                                     // 읽기 전용 트랜잭션으로 정의
+
         TransactionStatus txStatus = transactionManager.getTransaction(txDefinition);       // 트랜잭션 메니져에게 트랜잭션을 요청.
                                                                                             // 기존에 시작된 트랜잭션이 없으니, 새로운 트랜잭션을 시작시키고 정보를 반환.
                                                                                             // 동시에 만들어진 트랜잭션을 다른 곳에서도 사용할수 있도록 동기화 함.
