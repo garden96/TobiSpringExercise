d3a0ece97619cf7eec0e66f19e5ef63b587a5581
jeongown.an <garden96@gmail.com>
2021-02-21 07:47:33 +0900

    @Component annotation을 @Repository annotation으로 대체
      - @Repository annotation은 DAO 기능을 제공하는 클래스를 위한 @Component annotation 의 meta annotaion 이다.

--- src/springbook/user/dao/UserDaoJdbc.java
+++ src/springbook/user/dao/UserDaoJdbc.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 import springbook.user.sqlservice.SqlService;
@@ -13,7 +14,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-@Component
+@Repository
 public class UserDaoJdbc implements UserDao {
 
     @Autowired
