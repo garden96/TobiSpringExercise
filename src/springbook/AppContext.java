42ec2dc94450685b8dfda9b8d9543bd21be2b97b
jeongown.an <garden96@gmail.com>
2021-02-27 03:19:31 +0900

    TestAppContext 와 ProductionAppContext 을 AppContext 의 중첩클래스로 이동
     - TestAppContext 와 ProductionAppContext 클래스는 각각 독립적으로 사용될 수 있도록 static class로 변경
     - 기존의 클래스 파일은 삭제
     - @Import annotation 사용부분의 class도 중첩 클래스 형태로 변경
     - 중첩 클래스를 이용한 구성은 중첩 클래스내의 내용이 많지않아 중첩클래스로 변경하더라도 가독성이 떨어지지 않을 때 용이함

--- src/springbook/AppContext.java
+++ src/springbook/AppContext.java
@@ -1,19 +1,21 @@
 package springbook;
 
 import com.mysql.jdbc.Driver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
+import springbook.user.service.DummyMailSender;
+import springbook.user.service.UserService;
+import springbook.user.service.UserServiceTest;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "springbook.user")
-@Import({SqlServiceContext.class, TestAppContext.class, ProductionAppContext.class})
+@Import({SqlServiceContext.class, AppContext.TestAppContext.class, AppContext.ProductionAppContext.class})
 public class AppContext {
 
     /**
@@ -36,4 +38,31 @@ public class AppContext {
         return tm;
     }
 
+
+    @Configuration
+    @Profile("test")
+    public static class TestAppContext {
+
+        @Bean
+        public UserService testUserService() {
+            return new UserServiceTest.TestUserService();
+        }
+
+        @Bean
+        public MailSender mainSender() {
+            return new DummyMailSender();
+        }
+    }
+
+    @Configuration
+    @Profile("production")
+    public static class ProductionAppContext {
+
+        @Bean
+        public MailSender mailSender() {
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost("localhost");
+            return mailSender;
+        }
+    }
 }
